d2f2a0bb01af6abb4fb56b2ab1f6617ad135a7b4
Vincent Joy <vincentjoyhere@gmail.com>
2025-02-04 12:18:40 +0530

    added the admin dashboard setting s to see users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['email', 'username', 'is_staff', 'is_verified', 'created_at']
+    list_filter = ['is_staff', 'is_verified']
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_verified')}),
+        ('Important dates', {'fields': ('last_login', 'created_at', 'updated_at')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active', 'is_verified')
+        }),
+    )
+    search_fields = ['email', 'username']
+    ordering = ['email']
+
+admin.site.register(User, CustomUserAdmin)
